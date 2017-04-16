048718d7bef7af1f15609f6b84c68af4645903a2
David Waller <david@krawaller.se>
2017-04-16 13:40:43 +0200

    fix foekiller

--- tests/heroskills/foekiller.spec.ts
+++ tests/heroskills/foekiller.spec.ts
@@ -1,64 +1,59 @@
 import * as test from "tape";
-import { makeMonster, makeHero, calcRes } from '../testutils';
-
-import { BattleState } from '../../src/interfaces';
-import { calculate_damage_vs_monster } from '../../src/calculate/calculate_damage_vs_monster';
+import { makeMonster, makeHero, execUntil } from '../testutils';
+import { BattleState, FlowPerformHeroAttack } from '../../src/interfaces';
+import { monsters } from '../../src/library';
 
 test('the foekiller hero skill', t => {
   const battle: BattleState = {
     heroes: {
-      foekiller: makeHero('bloodsportBrawler',{stance:'assault'},{},{foeKiller: true})
+      hero: makeHero('bloodsportBrawler',{attackDice:[1,3]},{},{foeKiller: true})
     },
     monsters: {
       puny: makeMonster('slitherFish'),
       hasVal3: makeMonster('shambler')
-    }
+    },
+    log: []
   };
 
+  let result: BattleState;
+  let heroId = 'hero';
+  let attack = {}
+  let DMG = 3;
+
+
+  battle.heroes.hero.vars.stance = 'assault';
+  battle.heroes.hero.vars.target = 'hasVal3';
+  result = execUntil(battle, <FlowPerformHeroAttack>['flow','performHeroAttack',{heroId,attack}]);
   t.equal(
-    calculate_damage_vs_monster(battle, {
-      monsterId: 'hasVal3',
-      heroId: 'foekiller',
-      heroATK: calcRes(6),
-      monsterARM: calcRes(4)
-    }).value,
-    3,
+    result.monsters.hasVal3.vars.HP,
+    monsters.shambler.stats.HP - (DMG + 1 - monsters.shambler.stats.ARM),
     'foeKiller gives 1 additional damage against biggie in assault'
   );
 
+  battle.heroes.hero.vars.target = 'puny';
+  result = execUntil(battle, <FlowPerformHeroAttack>['flow','performHeroAttack',{heroId,attack}]);
   t.equal(
-    calculate_damage_vs_monster(battle, {
-      monsterId: 'hasVal3',
-      heroId: 'foekiller',
-      heroATK: calcRes(4),
-      monsterARM: calcRes(4)
-    }).value,
-    0,
-    'foeKiller has no effect when we didnt do any damage'
-  );
-
-  t.equal(
-    calculate_damage_vs_monster(battle, {
-      monsterId: 'puny',
-      heroId: 'foekiller',
-      heroATK: calcRes(6),
-      monsterARM: calcRes(4),
-    }).value,
-    2,
+    result.monsters.puny.vars.HP,
+    monsters.slitherFish.stats.HP - (DMG - monsters.slitherFish.stats.ARM),
     'no extra against puny enemy'
   );
 
-  battle.heroes.foekiller.vars.stance = 'defence';
-
+  battle.heroes.hero.vars.target = 'hasVal3';
+  battle.heroes.hero.vars.stance = 'defence';
+  result = execUntil(battle, <FlowPerformHeroAttack>['flow','performHeroAttack',{heroId,attack}]);
   t.equal(
-    calculate_damage_vs_monster(battle, {
-      monsterId: 'hasVal3',
-      heroId: 'foekiller',
-      heroATK: calcRes(6),
-      monsterARM: calcRes(4)
-    }).value,
-    2,
-    'foeKiller has no effect when not in assault mode'
+    result.monsters.hasVal3.vars.HP,
+    monsters.shambler.stats.HP - (DMG - monsters.shambler.stats.ARM),
+   'foeKiller has no effect when not in assault mode'
+  );
+  
+  battle.heroes.hero.vars.stance = 'assault';
+  battle.heroes.hero.vars.attackDice = [0,0];
+  result = execUntil(battle, <FlowPerformHeroAttack>['flow','performHeroAttack',{heroId,attack}]);
+  t.equal(
+    result.monsters.hasVal3.vars.HP,
+    monsters.shambler.stats.HP,
+   'foeKiller has no effect when we didnt do any damage'
   );
 
   t.end();
