e8bbea46664692631f4168f41ec7e1d962f24706
David Waller <david@krawaller.se>
2017-04-16 15:28:36 +0200

    update horde

--- tests/monsterskills/horde.spec.ts
+++ tests/monsterskills/horde.spec.ts
@@ -1,46 +1,60 @@
 import * as test from "tape";
-import { makeMonster } from '../testutils';
+import { makeMonster, makeHero, execUntil } from '../testutils';
 
-import { BattleState } from '../../src/interfaces';
+import { BattleState, EvilAttack, FlowPerformMonsterAttack } from '../../src/interfaces';
 import { monsters } from '../../src/library';
 import { calculate_monster_attack } from '../../src/calculate/calculate_monster_attack';
 
 test('horde monster skill', t => {
+  const startingHP = 10;
+  const DEF = 2;
   const battle: BattleState = {
-    heroes: {},
+    heroes: {
+      hero: makeHero('bloodsportBrawler',{HP:startingHP,defenceDice:[1,DEF]})
+    },
     monsters: {
-      nacht: makeMonster('nachtDrekSlicer'), // Has horde(weird)
+      nacht: makeMonster('nachtDrekSlicer',{target:'hero'}), // Has horde(weird)
       rat: makeMonster('ratThing'), // has weird
       slither: makeMonster('slitherFish') // doesnt have weird
-    }
+    },
+    log: []
   };
+  let result: BattleState;
+  
+  let monsterId = 'nacht';
+  let attack:EvilAttack = {type: 'regular'};
+  const ATK = monsters.nachtDrekSlicer.stats.ATK;
 
+  result = execUntil(battle, <FlowPerformMonsterAttack>['flow','performMonsterAttack',{monsterId,attack}]);
   t.equal(
-    calculate_monster_attack(battle, {monsterId: 'nacht'}).value,
-    monsters.nachtDrekSlicer.stats.ATK + 1,
+    result.heroes.hero.vars.HP,
+    startingHP - (ATK + 1 - DEF),
     'got 1 extra since ratThing is weird'
   );
 
   battle.monsters.rat.vars.killedBy = 'foo';
+  result = execUntil(battle, <FlowPerformMonsterAttack>['flow','performMonsterAttack',{monsterId,attack}]);
   t.equal(
-    calculate_monster_attack(battle, {monsterId: 'nacht'}).value,
-    monsters.nachtDrekSlicer.stats.ATK,
+    result.heroes.hero.vars.HP,
+    startingHP - (ATK - DEF),
     'no extra for dead monster'
   );
 
   delete battle.monsters.rat.vars.killedBy;
   battle.monsters.rat.vars.escaped = true;
+  result = execUntil(battle, <FlowPerformMonsterAttack>['flow','performMonsterAttack',{monsterId,attack}]);
   t.equal(
-    calculate_monster_attack(battle, {monsterId: 'nacht'}).value,
-    monsters.nachtDrekSlicer.stats.ATK,
+    result.heroes.hero.vars.HP,
+    startingHP - (ATK - DEF),
     'no extra for escaped monster'
   );
 
   delete battle.monsters.rat;
+  result = execUntil(battle, <FlowPerformMonsterAttack>['flow','performMonsterAttack',{monsterId,attack}]);
   t.equal(
-    calculate_monster_attack(battle, {monsterId: 'nacht'}).value,
-    monsters.nachtDrekSlicer.stats.ATK,
-    'no extra when no other weirdo'
+    result.heroes.hero.vars.HP,
+    startingHP - (ATK - DEF),
+    'no extra if no other weirdos'
   );
 
   t.end();
