29aa1e78a168928325e8ba4d02a28dac152cccf6
David Waller <david@krawaller.se>
2017-04-16 15:44:52 +0200

    update tests for dazed and corroded

--- tests/monsterstates/corroded.spec.ts
+++ tests/monsterstates/corroded.spec.ts
@@ -1,20 +1,27 @@
 import * as test from "tape";
-import { makeMonster } from '../testutils';
-import { BattleState } from '../../src/interfaces';
+import { makeMonster, makeHero, execUntil } from '../testutils';
+import { BattleState, FlowPerformHeroAttack } from '../../src/interfaces';
 import { monsters } from '../../src/library';
 import { calculate_monster_armour } from '../../src/calculate/calculate_monster_armour';
 
 test('corroded monsters', t => {
   const battle: BattleState = {
-    heroes: {},
+    heroes: {
+      hero: makeHero('bloodsportBrawler',{target: 'corroded', attackDice:[1,3]})
+    },
     monsters: {
       corroded: makeMonster('slitherFish',{},{corroded:true})
-    }
+    },
+    log: []
   };
 
+  let result:BattleState = execUntil(battle, <FlowPerformHeroAttack>['flow','performHeroAttack',{
+    heroId: 'hero', attack: {type:'special'}
+  }]);
   t.equal(
-    calculate_monster_armour(battle, {monsterId: 'corroded'}).value,
-    0, 'corroded means arm is 0'
+    result.monsters.corroded.vars.HP,
+    monsters.slitherFish.stats.HP - 3,
+    'corroded means arm is 0'
   );
 
   t.ok( monsters[battle.monsters.corroded.blueprint].stats.ARM, 'monster would have had armour otherwise' );
--- tests/monsterstates/dazed.spec.ts
+++ tests/monsterstates/dazed.spec.ts
@@ -1,27 +1,41 @@
-import * as test from "tape";
-import { lastLogHasStr, makeMonster } from '../testutils';
+/*
+State caused by Dimwit skill
+*/
 
-import { BattleState } from '../../src/interfaces';
+import * as test from "tape";
+import { lastLogHasStr, makeMonster, makeHero, execUntil } from '../testutils';
+import { monsters } from '../../src/library';
+import { BattleState, FlowPerformMonsterAttack, EvilAttack } from '../../src/interfaces';
 import { apply_end_of_round_to_monster } from '../../src/apply/apply_end_of_round_to_monster';
-import { calculate_monster_attack } from '../../src/calculate/calculate_monster_attack';
 
 test('the dazed monster state', t => {
+
+  const startingHP = 10;
+  const DEF = 2;
   let battle: BattleState = {
-    heroes: {},
+    heroes: {
+      hero: makeHero('bloodsportBrawler',{HP:10,defenceDice:[1,DEF]})
+    },
     monsters: {
-      monster: makeMonster('megaRat',{},{dazed: true}) // atk - 5
+      monster: makeMonster('megaRat',{target:'hero'},{dazed: true}) // atk 5
     },
     log: []
   };
+  const ATK = monsters.megaRat.stats.ATK;
+  const attack:EvilAttack = {type: 'regular'};
+  const monsterId = 'monster';
+  let result: BattleState;
 
+  result = execUntil(battle, <FlowPerformMonsterAttack>['flow','performMonsterAttack',{monsterId,attack}]);
   t.equal(
-    calculate_monster_attack(battle, {monsterId: 'monster'}).value,
-    2, 'attack is halved for monsters when dazed'
+    result.heroes.hero.vars.HP,
+    startingHP - (Math.floor(ATK/2) - DEF),
+    'attack is div 2'
   );
 
   battle = apply_end_of_round_to_monster(battle, {monsterId: 'monster'});
   t.ok(!battle.monsters.monster.states.dazed, 'dazed was removed')
-  t.ok(lastLogHasStr(battle, 'dazed'), 'message announced stun removal');
+  t.ok(lastLogHasStr(battle, 'aze'), 'message announced hung over removal');
 
   t.end();
 });
