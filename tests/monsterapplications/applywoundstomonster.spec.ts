1947a171749466d9ef9c386f3c49a936a6dde9d5
David Waller <david@krawaller.se>
2017-04-16 14:24:06 +0200

    update woundmosnterspec

--- tests/monsterapplications/applywoundstomonster.spec.ts
+++ tests/monsterapplications/applywoundstomonster.spec.ts
@@ -2,7 +2,7 @@ import * as test from "tape";
 import { lastLogHasStr, makeHero, makeMonster } from '../testutils';
 
 import { BattleState, LogMessagePart } from '../../src/interfaces';
-import { apply_wounds_to_monster } from '../../src/apply/apply_wounds_to_monster';
+import { apply_wounds_to_monster, WoundMonsterSpec } from '../../src/apply/apply_wounds_to_monster';
 
 test('apply wounds to monster', t => {
   let battle: BattleState = {
@@ -10,8 +10,8 @@ test('apply wounds to monster', t => {
     monsters: { monster: makeMonster('slitherFish',{HP: 7}) },
     log: []
   };
-  const blow = { heroId: 'hero', monsterId: 'monster', wounds: {value: 4, history: []} }
-  const failedBlow = { heroId: 'hero', monsterId: 'monster', wounds: {value: 0, history: []} }
+  const blow:WoundMonsterSpec = { heroId: 'hero', monsterId: 'monster', wounds: {value: 4, history: []}, attack: {type:'special'} }
+  const failedBlow:WoundMonsterSpec = { heroId: 'hero', monsterId: 'monster', wounds: {value: 0, history: []}, attack: {type:'special'} }
 
   battle = apply_wounds_to_monster(battle, failedBlow);
   t.equal(battle.monsters.monster.vars.HP, 7, 'failed blow deals no wounds to hero HP');
