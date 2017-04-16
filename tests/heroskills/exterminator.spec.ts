32aa0975e77ad008e3769e58969bf070a77ebca6
David Waller <david@krawaller.se>
2017-04-16 13:30:09 +0200

    fix exxterminator

--- tests/heroskills/exterminator.spec.ts
+++ tests/heroskills/exterminator.spec.ts
@@ -1,64 +1,59 @@
 import * as test from "tape";
-import { makeHero, makeMonster, calcRes } from '../testutils';
+import { makeHero, makeMonster, calcRes, execUntil } from '../testutils';
 
-import { BattleState } from '../../src/interfaces';
+import { BattleState, FlowPerformHeroAttack } from '../../src/interfaces';
 import { monsters } from '../../src/library';
-import { calculate_damage_vs_monster } from '../../src/calculate/calculate_damage_vs_monster';
 
 test('the Exterminator hero skill', t => {
   const battle: BattleState = {
     heroes: {
-      exterminator: makeHero('bloodsportBrawler',{stance: 'assault'},{},{exterminator:true}),
-      defendingExterminator: makeHero('bloodsportBrawler',{stance: 'defence'},{},{exterminator:true}),
+      hero: makeHero('bloodsportBrawler',{attackDice:[1,3]},{},{exterminator:true}),
     },
     monsters: {
       vermin: makeMonster('slitherFish'),
       nonvermin: makeMonster('manAtArms')
-    }
+    },
+    log: []
   };
 
+  let result: BattleState;
+  let heroId = 'hero';
+  let attack = {type:'meelee'};
+  let DMG = 3; // highest attack die
+
+  battle.heroes.hero.vars.stance = 'assault';
+  battle.heroes.hero.vars.target = 'vermin';
+  result = execUntil(battle, <FlowPerformHeroAttack>['flow','performHeroAttack',{heroId,attack}]);
   t.equal(
-    calculate_damage_vs_monster(battle, {
-      monsterId: 'vermin',
-      heroId: 'exterminator',
-      heroATK: calcRes(6),
-      monsterARM: calcRes(4)
-    }).value,
-    3,
+    result.monsters.vermin.vars.HP,
+    monsters.slitherFish.stats.HP - (DMG + 1 - monsters.slitherFish.stats.ARM),
     'exterminator gives 1 additional damage vs vermin'
   );
 
+  battle.heroes.hero.vars.target = 'nonvermin';
+  result = execUntil(battle, <FlowPerformHeroAttack>['flow','performHeroAttack',{heroId,attack}]);
   t.equal(
-    calculate_damage_vs_monster(battle, {
-      monsterId: 'vermin',
-      heroId: 'exterminator',
-      heroATK: calcRes(4),
-      monsterARM: calcRes(4)
-    }).value,
-    0,
-    'exterminator has no effect if didnt do damage'
+    result.monsters.vermin.vars.HP,
+    monsters.manAtArms.stats.HP - (DMG - monsters.manAtArms.stats.ARM),
+    'exterminator gives 1 additional damage vs vermin'
   );
 
+  battle.heroes.hero.vars.target = 'vermin';
+  battle.heroes.hero.vars.stance = 'defence';
+  result = execUntil(battle, <FlowPerformHeroAttack>['flow','performHeroAttack',{heroId,attack}]);
   t.equal(
-    calculate_damage_vs_monster(battle, {
-      monsterId: 'nonvermin',
-      heroId: 'exterminator',
-      heroATK: calcRes(6),
-      monsterARM: calcRes(4)
-    }).value,
-    2,
-    'exterminator has no effect against non-vermin'
-  );
-
-  t.equal(
-    calculate_damage_vs_monster(battle, {
-      monsterId: 'vermin',
-      heroId: 'defendingExterminator',
-      heroATK: calcRes(6),
-      monsterARM: calcRes(4)
-    }).value,
-    2,
+    result.monsters.vermin.vars.HP,
+    monsters.slitherFish.stats.HP - (DMG - monsters.slitherFish.stats.ARM),
     'defending exterminator has no effect'
+  );
+
+  battle.heroes.hero.vars.stance = 'assault';
+  battle.heroes.hero.vars.attackDice = [1, monsters.slitherFish.stats.ARM];
+  result = execUntil(battle, <FlowPerformHeroAttack>['flow','performHeroAttack',{heroId,attack}]);
+  t.equal(
+    result.monsters.vermin.vars.HP,
+    monsters.slitherFish.stats.HP,
+    'exterminator has no effect if didnt do damage'
   );
 
   t.end();
