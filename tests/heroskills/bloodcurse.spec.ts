8d9b8f0087bfdd1c544e3835b08b07d98e8088f8
David Waller <david@krawaller.se>
2017-04-18 07:24:27 +0200

    update bloodcurse spec

--- tests/heroskills/bloodcurse.spec.ts
+++ tests/heroskills/bloodcurse.spec.ts
@@ -9,13 +9,13 @@ import { heroSkills } from '../../src/library';
 test('blood curse hero skill', t => {
   let battle: BattleState = {
     heroes: {
-      curseLinkedHero: makeHero('bloodsportBrawler',{HP: 5},{},{bloodCurse:true}),
+      curseLinkedHero: makeHero('bloodsportBrawler',{HP: 9, failedDefence: true},{},{bloodCurse:true}),
       anotherHero: makeHero('infamousButcher',{HP: 7})
     },
     monsters: {
-      cursedMonster: makeMonster('slitherFish',{HP:3}),
-      nextMonster: makeMonster('manAtArms',{HP:5}),
-      thirdMonster: makeMonster('manAtArms')
+      cursedMonster: makeMonster('slitherFish',{HP:5,target:'curseLinkedHero'}),
+      nextMonster: makeMonster('imperialHuntsman',{HP:5,target:'curseLinkedHero'}), // ATK 4
+      thirdMonster: makeMonster('imperialHuntsman',{target:'curseLinkedHero'}) // ATK 4
     },
     log: [],
     seed: 'bloodcursetest' // 4 6 3 1
@@ -27,7 +27,7 @@ test('blood curse hero skill', t => {
     'bloodcurse not available in defence stance'
   );
 
-  // TEST THROWING
+  // ---------------- TEST THROWING
 
   battle.heroes.curseLinkedHero.vars.stance = 'assault';
   battle = execUntil(battle, ['flow','selectAction',{heroId:'curseLinkedHero'}]);
@@ -45,35 +45,22 @@ test('blood curse hero skill', t => {
   t.equal(battle.monsters.cursedMonster.states.bloodCurse, 'curseLinkedHero', 'blood curse state added to monster');
   t.ok(lastLogHasStr(battle, 'lood curse'), 'msg acknowledges the successful');
 
-  // TEST EFFECT
-
-  battle = apply_wounds_to_hero(battle, {
-    heroId: 'curseLinkedHero',
-    monsterId: 'thirdMonster',
-    wounds: {value: 2, history: []}
-  });
-  t.equal(battle.heroes.curseLinkedHero.vars.HP, 3, 'hero took damage as normal');
-  t.equal(battle.monsters.cursedMonster.vars.HP, 1, 'cursed monster took same damage');
+  // ---------------- TEST EFFECT
+  
+  battle = execUntil(battle, ['flow','performMonsterAttack',{monsterId:'thirdMonster'}]); // will do 4 DMG to hero
+  t.equal(battle.heroes.curseLinkedHero.vars.HP, 5, 'hero HP is now 9-4=5');
+  t.equal(battle.monsters.cursedMonster.vars.HP, 1, 'cursed monster took same damage (5-4=1)');
   t.ok(lastLogHasStr(battle, 'lood curse'), 'msg acknowledges the curse');
 
-  battle = apply_wounds_to_hero(battle, {
-    heroId: 'curseLinkedHero',
-    monsterId: 'thirdMonster',
-    wounds: {value: 2, history: []}
-  });
-  t.equal(battle.heroes.curseLinkedHero.vars.HP, 1, 'hero took damage as normal');
+  battle = execUntil(battle, ['flow','performMonsterAttack',{monsterId:'thirdMonster'}]); // will knock monster out
+  t.equal(battle.heroes.curseLinkedHero.vars.HP, 1, 'hero HP is now 5-4=1');
   t.equal(battle.monsters.cursedMonster.vars.HP, 0, 'monster was knocked out by curse');
-  t.ok(!battle.heroes.curseLinkedHero.vars.bloodCurseLink, 'blood curse link was removed');
   t.ok(!battle.monsters.cursedMonster.states.bloodCurse, 'blood curse state was removed from monster');
   t.ok(lastLogHasStr(battle, 'lifted'), 'msg says curse was lifted');
 
   battle.heroes.curseLinkedHero.vars.bloodCurseLink = 'nextMonster';
   battle.monsters.nextMonster.states.bloodCurse = 'curseLinkedHero';
-  battle = apply_wounds_to_hero(battle, {
-    heroId: 'curseLinkedHero',
-    monsterId: 'thirdMonster',
-    wounds: {value: 2, history: []}
-  });
+  battle = execUntil(battle, ['flow','performMonsterAttack',{monsterId:'thirdMonster'}]); // will knock hero out
   t.equal(battle.heroes.curseLinkedHero.vars.HP, 0, 'hero was knocked out as he should have been');
   t.equal(battle.monsters.nextMonster.vars.HP, 4, 'cursed monster only took 1 HP, since that is what was left for hero');
 
