dfd8eb2c65020dc45cab6c88145d4dabdf9cd0d7
David Waller <david@krawaller.se>
2017-05-02 12:55:29 +0200

    fix

--- engine/src/apply/apply_escape_outcome.ts
+++ engine/src/apply/apply_escape_outcome.ts
@@ -9,7 +9,7 @@ export interface EscapeOutcomeSpec {
 export function apply_escape_outcome_to_hero(battle: BattleState, {heroId}:EscapeOutcomeSpec): BattleState {
   let ret:BattleState = deepCopy(battle);
   ret = apply_dice_removal(ret, {heroId: heroId, diceType: {attack:true, power: true, defence: true}}); // TODO - really?
-  ret.heroes.hero.vars.escaped = true;
+  ret.heroes[heroId].vars.escaped = true;
   ret = addLog(ret, [{heroRef:heroId},'manages to escape the battle!']);
   return ret;
 }
