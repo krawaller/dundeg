81d71ea7f123207366ecc3081cd1454b8d0d1c4f
David Waller <david@krawaller.se>
2017-05-03 22:00:16 +0200

    index

--- app/components/app.tsx
+++ app/components/app.tsx
@@ -14,10 +14,14 @@ export const App = withBattle(props=>{
         </div>
         <div>
           <h4>ToDo</h4>
-          <p>Updated <strong>17-05-02</strong>.</p>
+          <p>Updated <strong>17-05-03</strong>.</p>
           <ul>
             <li>add Quick Actions</li>
             <li>add Pain enemy skill</li>
+            <li>add infrastructure for hero entry skills</li>
+            <li>add Provoke skill</li>
+            <li>add Ferocious charge</li>
+            <li>add Sneak attack</li>
             <li className="done">add default unarmed attack</li>
             <li className="done">limit Find Weakness to 1 enemy at a time</li>
             <li className="done">change Escape to new rules</li>
